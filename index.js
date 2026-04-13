0032daf2c15757f55f90e7bcca762da0beb26d89
allyzagwennsuana202301889-bit <allyzagwennsuana.202301889@gmail.com>
2026-04-13 11:19:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(_dirname, 'public')))
+
+const students = [
+{
+  id: 1,
+  name: "john doe",
+  course: "BSIT"
+},
+{
+  id: 2,
+  name: "jane doe",
+  course: "BSCS"
+},
+{
+  id: 3,
+  name: "liam doe",
+  course: "BSIT"
+}
+]
+app.get('/api/students',(req, res) => {
+  res.json(students)
+})
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+}
